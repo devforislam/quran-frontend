2f2075305730dbf1b87d13de997f17f928078bdc
Mahbub Rabbani <mahbub.rucse@gmail.com>
2020-01-16 09:57:01 +0600

    add hadith page

---
+++ src/pages/hadith.js
@@ -0,0 +1,10 @@
+
+import styles from './hadith.css';
+
+export default function() {
+  return (
+    <div className={styles.textCenter}>
+      <h1>Insha-Allah, This feature will be launched soon.</h1>
+    </div>
+  );
+}
